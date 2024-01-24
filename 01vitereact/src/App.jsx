a034e3548632bba08d70c5d90f81555b47de5f45
Meet <meetmavani14@gmail.com>
2024-01-24 13:17:37 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Temp from "./jsr"
+
+function App() {
+  const username = '!'
+
+  return (
+    <>
+    <h1>Jai Shree Ram | 22/01/24 {username}</h1>  //evaluated expression
+   <Temp/>
+  </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/jsr.jsx
@@ -0,0 +1,7 @@
+function Temp(){
+    return(
+        <h3>Jai Shree Ram</h3>
+    )
+}
+
+export default Temp
